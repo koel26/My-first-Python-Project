4092af2975be9184bf67107c2fde5784d8db46eb
koel26 <51009536+koel26@users.noreply.github.com>
2020-01-31 09:24:05 -0500

    Create Delay.py

---
+++ Delay.py
@@ -0,0 +1,3 @@
+import pandas as pd
+df = pd.read_csv('C:/Users/Aheli/Desktop/Harrishberg University/Courses/Sem 3/ANLY 560 Functional programming methods/Assignments/First project on Python/flight-delays/flights.csv', low_memory=False)
+print('Dataframe dimensions:', df.shape)
